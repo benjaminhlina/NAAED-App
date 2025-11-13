6de255572a8263cd13905231b90affc61648529c
benjaminhlina <benjamin.hlina@gmail.com>
2025-11-13 09:21:46 -0500

    move stuff to summary_sidebar_server()

--- modules/view_summer_info.R
+++ modules/view_summer_info.R
@@ -66,61 +66,7 @@ summary_info_server <- function(id, con, main_input) {
       }
     })
     # ----- Dynamically Update Sidebar Dropdowns -----
-    # Get numeric columns globally
-    numeric_cols <- reactive({
-      df <- summary_data()
-      req(df)
-      get_numeric_cols(df)
-    })
-    # ----- get groupsing columns
-    grouping_cols <- reactive({
-      df <- summary_data()
-      req(df)
-      get_good_groups(df)
-    })
 
-    observeEvent(summary_data(), {
-
-      cat("\n[DEBUG] Updating dropdowns...\n")
-      # df is summary data
-      df <- summary_data()
-      req(df)
-      cat("[DEBUG] Waterbody unique values:", length(unique(df$Waterbody)), "\n")
-      cat("[DEBUG] Species unique values:", length(unique(df$`Common Name`)), "\n")
-      # Grouping Variables: Allow dynamic selection
-
-      grouping_choices <- grouping_cols()
-
-      updateSelectInput(session, "summary_grouping_vars",
-                        choices = grouping_choices,
-                        selected = c("Waterbody",
-                                     "Common Name")
-      )
-      #
-      # Waterbody Drop-down
-      updateSelectInput(session, "summary_waterbody_filter",
-                        choices = c("All", sort(unique(df$Waterbody))),
-                        selected = "All")
-
-      # Species Drop-down
-      updateSelectInput(session, "summary_species_filter",
-                        choices = c("All", sort(unique(df$`Common Name`))),
-                        selected = "All")
-      # Get numeric column choices
-      numeric_choices <- numeric_cols()
-      # Update histogram variable choices
-      updateSelectizeInput(session, "hist_var",
-                           choices = setNames(numeric_choices, numeric_choices),
-                           # selected = "test",
-                           server = TRUE)
-      # Update histogram variable choices
-      updateSelectizeInput(session, "summary_y_variable",
-                           choices = setNames(numeric_choices, numeric_choices),
-                           # selected = "test",
-                           server = TRUE)
-    })
-    #
-    #
     # # ---- Generate Summary Statistics with Dynamic Grouping -----
     filtered_summary_df <- reactive({
       df <- summary_data()
