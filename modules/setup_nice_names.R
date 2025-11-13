d9e4c612bcd86e7f5f89f24b8b39ebb6e800ebf8
benjaminhlina <benjamin.hlina@gmail.com>
2025-11-13 09:00:41 -0500

    Move setup nice names to moduales

---
+++ modules/setup_nice_names.R
@@ -0,0 +1,24 @@
+# ---- create renaming table ----
+{
+  library(dplyr)
+  library(here)
+  library(readr)
+}
+
+
+naming_conventions <- read_csv(here::here("data",
+                                          "app_naming_conventions.csv")) |>
+  mutate(
+    nice_names = case_when(
+      raw_names %in% "d13c" ~ "\U03B4<sup>13</sup>C",
+      raw_names %in% "d15n" ~ "\U03B4<sup>15</sup>N",
+      raw_names %in% "d34s" ~ "\U03B4<sup>34</sup>S",
+      .default = nice_names
+    )
+  )
+
+# print(naming_conventions, n = 50)
+
+# create named vectors
+nice_name_lookup <- setNames(naming_conventions$nice_names,
+                             naming_conventions$raw_names)
