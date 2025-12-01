427aa937182e5f9ba2964b5f57593a3f92919300
benjaminhlina <benjamin.hlina@gmail.com>
2025-12-01 15:35:21 -0500

    first commmit of fix_var_generic

--- modules/helpers.R
+++ modules/helpers.R
@@ -1,3 +1,38 @@
+fix_var_generic <- function(df, var_raw, get_nice_name) {
+
+
+  # detect if it's one of the synthetic length vars
+  if (grepl("^length_mm__", var_raw)) {
+    # split if length_mm__ is presnet in
+
+    parts <- strsplit(var_raw, "__")[[1]]
+    # grab the second element of part
+    var_type <- parts[2]
+
+    # Filter df to matching length type
+    df <- df %>%
+      dplyr::filter(length_type == var_type)
+
+    # Dynamic label
+    var_label <- paste0(stringr::str_to_title(var_type), " Length (mm)")
+    var <- "Length (mm)"
+
+  } else {
+
+    req(var_raw %in% names(df))
+    # Normal variable
+    var <- var_raw
+    var_label <- get_nice_name(var)[[1]]
+  }
+
+  list(
+    df = df,
+    var = var,
+    var_label = var_label
+  )
+
+}
+
 # ---- get good groups -----
 get_good_groups <- function(df) {
 good_groups <- c(
